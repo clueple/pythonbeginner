979d5d245219cc26d15faca68a57d44f0912d29e
clueple <clueple@gmail.com>
2022-08-24 12:54:19 +0800

    Add files via upload
    added 'save as file' in addition to converting essays into multiple lines of voice over scripts

--- essay_to_scripts.py
+++ essay_to_scripts.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import customtkinter as ck
 import re
+from tkinter import filedialog
 
 #### Variables  ##
 title_txt = 'Scripts Converter'
@@ -22,6 +23,17 @@ def convert():
 	# print(output_txt.insert(END, result))
 	output_txt.insert(END,result)
 
+def save_file():
+	result = re.sub(pattern, rep, input_txt.get(1.0, END))
+	file = filedialog.asksaveasfilename(
+		filetypes=[('text file', '*.txt'), ('CSV file', '*.csv')],
+		defaultextension= '.txt',
+		#initialdir = r'D:/',
+		title = 'Save As'
+		)
+	fob = open(file,'w')
+	fob.write(f"{result}")
+	fob.close()
 
 
 #### Elements  ##
@@ -47,21 +59,24 @@ button2 = ck.CTkButton(
 	command = lambda: input_txt.delete(1.0, END),
 	text = 'Clear'
 	)
+button3 = ck.CTkButton(
+	master = root,
+	command = save_file
+	,
+	text = 'Export To File'
+	)
 
-
-# output_txt = ck.CTkLabel(
-# 	master=root,
-# 	textvariable=src_txt,
-# 	text='Click to Convert'
-# 	)
 output_txt = Text(root)
 
 
+
 ### layout  ###
-input_txt.pack(pady=10,padx=10)
-button1.pack(pady=10, padx=10)
-output_txt.pack(pady=10, padx=10)
-button2.pack(pady=10,padx=10)
+button1.grid(row=0,column=0)
+button2.grid(row=0,column=1)
+button3.grid(row=0,column=2)
+input_txt.grid(row=1, columnspan=3)
+output_txt.grid(row=2, columnspan=3)
+
 
 
 
