0e4e658ade6f3bba40b6a52a5ad9b7515ad83d52
clueple <clueple@gmail.com>
2022-09-29 15:58:12 +0800

    Add files via upload
    This is the template file of the pytube project - 'YouTube Downloader'; basic elements such as 'entry', 'button', 'label', and 'url' (StringVar) are provided.  
    The learner's task is to change the 'button' to download video from the 'url' specified in the 'entry' widget.

--- pytube_app.py
+++ pytube_app.py
@@ -1,32 +1,53 @@
 # import customtkinter as ck 
 # import re 
 from tkinter import filedialog 
-import tkinter as tk 
+from tkinter import *
+from tkinter import ttk
 
 """Variables"""
 title_text = 'Example App'
-win_size = "840x480"
+win_size = "640x480"
 
 
 """ Elements """
 # app = ck.CTk()
-app = tk.Tk()
+app = Tk()
 app.title(title_text)
 app.geometry(win_size)
 
-
 """click event """
-url_input = 'whatever'
+def download():
+	entry.delete(0, END)
+	entry.insert(END, 'insert video url here')
+	label.configure(text=f"You have downloaded from: {entry.get()}")
 
-"""elements"""
-url_entry = ttk.Entry(app, textvariable=url_input, width=20, height=5)
+"""Elements"""
+
+#step 2 --- create entry widget, with the 'url' as the textvariable of entry widget
+url=StringVar()
+entry = Entry(app, width=50, textvariable=url)   
+#step 3 --- create h_scrollbar set command to bind to entry widget
+h_scrollbar = Scrollbar(app, orient='horizontal' , command=entry.xview)
+#step 6 --- create a button to download video from the link specified in 'entry' widget
+button = Button(app, text='Download', activebackground='white', activeforeground='black', command=download)
+label = Label(app)
+
+
+"""teting the entry field """
+
+""" binding """
+#step 4 --- bind the entry widget to the h_scrollbar
+entry.config(xscrollcommand=h_scrollbar.set)
 
 """layout"""
-url_entry.grid(row=0, column=1)
+#step 5 --- set layout for entry, and h_scrollbar; remember to set the h_scrollbar's position below the entry
+entry.grid(row=0,column=0)
+h_scrollbar.grid(row=1, column=0, sticky='we')
+button.grid(row=2,column=0, sticky='we')
+label.grid(row=3,column=0,sticky='nsew')
 
-
+"""run program"""
 app.mainloop()
-
 
 
 # url = r"https://youtu.be/00yGusN2-Uw"
