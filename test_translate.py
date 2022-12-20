1932f223bd5fc5145b5147919b7640424ddf1a38
clueple <clueple@gmail.com>
2022-12-20 18:34:31 +0800

    Add files via upload
    The template to create a translator app with ttkbootstrap:
    
    Major task to accomplish: translate the English phrase to 
    Italian, French, German, Japanese, Chinese, and Spanish

--- test_translate.py
+++ test_translate.py
@@ -1,8 +1,12 @@
 import pyjyutping as jtc 
 from tabulate import tabulate 
-import customtkinter as ctk 
+# import customtkinter as ctk 
 from tkinter import *
+# import tkinter as tk 
 from googletrans import Translator
+
+import ttkbootstrap as btk
+from ttkbootstrap.constants import *  
 
 """ Backend Settings """
 # pattern to be used:
@@ -11,78 +15,64 @@ title_txt = "Warren's Translation"
 win_size = "720x640"
 label_select_font_txt = "Select Font"
 
-ctk.set_appearance_mode("system") # Modes: "System" (standard), "Dark", "Light"
-ctk.set_default_color_theme("green") # Themes: "blue" (standard), "green", "dark-blue"
+# ctk.set_appearance_mode("system") # Modes: "System" (standard), "Dark", "Light"
+# ctk.set_default_color_theme("green") # Themes: "blue" (standard), "green", "dark-blue"
 
 # create a google translator
 trans = Translator()
 
-app = ctk.CTk()
+app = btk.Window()
+# app = btk.Window(themename='minty')
+# app = ctk.CTk()
 app.geometry(win_size)
 app.title(title_txt)
 
-# functions
-# press the translate button to translate whatever entered in entry1 to English
-def press_translate():
-	# src_text = entry1.get()
-	# lang_detected = trans.detect(src_text).lang
-	# trans_txt = trans.translate(entry1.get(), dest='en')
-	# output1.delete(1.0, END)
-	# output1.insert(END, trans_txt.text)
-	lang_list = ['it', 'de', 'fr', 'ja', 'zh-TW', 'es']
-	color_list = ['purple', 'black', 'light blue', 'dark blue', 'green', 'red']
-	d_lang_list = ['Italiano', 'Deutsch', 'Francase', '日本語', '廣東話', 'Espanol']
+lang = {
+	'name': ['English', 'Italian', 'German', 'French', 'Japanese', 'Chinese', 'Spanish'],
+	"code": ['en', 'it', 'de', 'fr', 'ja', 'zh-TW', 'es'],
+	"color": ['red','purple', 'black', 'light blue', 'dark blue', 'green', 'brown'],
+	"dname": ['English','Italiano', 'Deutsch', 'Français', '日本語', '廣東話', 'Español']
+}
 
 
-	src_txt = entry1.get()
-	lang_detect = trans.detect(src_txt).lang 
-	for i in range(len(lang_list)):
-		frame = ctk.CTkFrame(app, fg_color=color_list[i])
-		label=ctk.CTkLabel(frame, text=d_lang_list[i], text_color='white')
-		output=ctk.CTkTextbox(frame, width=200, height=25)
+my_themes=app.style.theme_names() #list of themes available
 
-		frame.pack(padx=20, pady=10, fill=X, expand=True)
-		label.pack(padx=10, pady=10, side=LEFT)
-		output.pack(padx=10, pady=10, fill=X, expand=True)
+"""create style selection"""
+r,c = 0,0 #row, 'r', column, 'c' equals 0
+my_str = btk.StringVar(value= app.style.theme_use())
 
+for values in my_themes: #list of all the themes
+	radio_btn = btk.Radiobutton(app, text=values, variable=my_str, value=values, command= lambda:my_update())
+	radio_btn.grid(row=r, column=c, padx=5, pady=20)
+	c = c+1 #increase column by 1
+	if c>8:
+		r,c = r+1, 0
 
+c,r = 0, r+1
+for my_style in app.style.colors: # list of styles
+	btn = btk.Button(app, text=my_style, bootstyle=my_style)
+	btn.grid(row=r, column=c, padx=1, pady=20)
+	c = c+1
 
-# translate whatever entered into the entry widget (entry1) to English
-def to_eng(e):
-	# entered text
-	enter_text = entry1.get()
-	enter_lang = trans.detect(enter_text)
-	preview_text = trans.translate(enter_text, dest='en')
-	output1.delete(1.0, END)
-	output1.insert(END, preview_text.text)
+def my_update():
+	app.style.theme_use(my_str.get())
 
+"""create elements"""
+entry_list = []
+entry_txt = StringVar(value='Enter text here')
+for i in range(len(lang['code'])):
+	label = btk.Label(app, text=f"{lang['name'][i]}")
+	label.grid(row=i+20, column=0, padx=10, pady=5)
 
+	entry = btk.Entry(app, font=20)
+	entry.grid(row=i+20 , column=1, pady=5, sticky=NE)
 
-# create frames
-frame1 = ctk.CTkFrame(app)
-frame2 = ctk.CTkFrame(app, fg_color='Red')
-
-
-# input text area
-entry1 = ctk.CTkEntry(frame1, width=600, height=25, placeholder_text='Enter Text Here', border_width=2, corner_radius=10)
-button = ctk.CTkButton(frame1, width=100, height=25, text='Translate', command=press_translate)
-
-label1 = ctk.CTkLabel(frame2, text='English', text_color='White')
-output1 = ctk.CTkTextbox(frame2, width=200, height=25)
-
-# layout
-frame1.pack(padx=20, pady=10, fill=X, expand=True)
-button.pack(padx=10, pady=10, side=RIGHT)
-entry1.pack(padx=20, pady=10, fill=X, expand=True)
-
-frame2.pack(padx=20, pady=10, fill=X, expand=True)
-label1.pack(padx=10, pady=10, side=LEFT)
-output1.pack(padx=10, pady=10, fill=X, expand=True)
-
-
+	entry_list.append(entry)
 
 # bind
-entry1.bind('<KeyRelease>', to_eng)
+
+
+"""create layout"""
 
 
 
