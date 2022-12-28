18a80f33842200b9ff8f1d2f515e5525e3086bde
clueple <clueple@gmail.com>
2022-12-28 13:17:03 +0800

    Add files via upload
    A template to create a translator app with Flet Python

--- flet_translate.py
+++ flet_translate.py
@@ -5,6 +5,7 @@ from tabulate import tabulate as tb
 from tabulate import tabulate_formats
 import re 
 from googletrans import Translator
+from flet import colors
 
 
 #pattern to split words
@@ -15,6 +16,13 @@ trans = Translator()
 
 """backend setting """
 title_txt = "Warren's Translator"
+
+lang = {
+    'name': ['English', 'Italian', 'German', 'French', 'Japanese', 'Chinese', 'Spanish'],
+    "code": ['en', 'it', 'de', 'fr', 'ja', 'zh-CN', 'es'],
+    "color": ['red','purple', 'black', 'light blue', 'dark blue', 'green', 'brown'],
+    "dname": ['English','Italiano', 'Deutsch', 'Français', '日本語', '廣東話', 'Español']
+}
 
 
 def main(page: ft.Page):
@@ -28,9 +36,16 @@ def main(page: ft.Page):
         table = [top_list, bot_list]
         result = tb(table, tablefmt='tsv')
         result_txt.value = result
+
+        src_lang = trans.detect(src_text.value).lang
+        src_lang_idx = lang['code'].index(src_lang)
+        result_txt.label = lang['dname'][src_lang_idx]
+
         page.update()
 
     eng_trans = ft.Text()
+
+
     # result_txt = ft.Text()
     result_txt = ft.TextField(label='Result',multiline=True, text_align='right')
     src_text = ft.TextField(label='Enter Text', on_change=get_result)
