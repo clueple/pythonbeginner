22375550994ecc9b2ad21eb765f30df2e9b37608
clueple <clueple@gmail.com>
2023-01-01 16:22:40 +0800

    Add files via upload
    A template Python file for Flutter app creation with Flet Python

--- flettest.py
+++ flettest.py
@@ -1,29 +1,59 @@
 import flet as ft 
 from pyjyutping import jyutping as jtc 
-import re 
-# from tabulate import tabulate as tb
-from prettytable import PrettyTable as pt
 from googletrans import Translator
-from prettytable import HEADER, ALL, FRAME
+import re 
 
+#translator object for human language translation
+trans = Translator
 
-pat = r"[-a-zA-Z0-9]+|[\S]"
-trans = Translator()
+lang ={
+	'code': ['en', 'it', 'de', 'ja', 'fr', 'zh-TW', 'es'],
+	'dname': ['English', 'Italiano', 'Deutsch', '日本語', 'Français', '廣東話', 'Español'],
+	'name':['English', 'Italian', 'German', 'Japanese', 'French', 'Cantonese', 'Spanish'],
+	'color':['red', 'purple', 'black', "#211790", 'blue', 'green', 'brown']
+}
 
-def main(page: ft.page):
-	def show_element3(e):
-		element3.value = f"You are writing this to element3: {e.control.value}"
-		page.update()
+def main(page: ft.Page):
+	"""basic settings"""
+	page.title = "Warren's app"
+	page.window_width = 108*6 #desktop app only
+	page.window_height = 135*6 #desktop app only
+	page.vertical_alignment = ft.MainAxisAlignment.CENTER
+	page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+	page.bgcolor='white'
 
+	"""events"""
+	
+	def items(count):
+		"""create containers that hold all the languages"""
+		items = []
+		for i in range (0, count):
+			items.append(
+				ft.Container(
+					content = ft.TextField(label=lang['dname'][i]),
+					alignment = ft.alignment.center,
+					width = 300,
+					height = 70,
+					bgcolor = lang['color'][i],
+					border_radius = ft.border_radius.all(5),
+					padding =20,
+					margin=10,
+					gradient = ft.LinearGradient(
+						begin=ft.alignment.top_center, 
+						end=ft.alignment.bottom_center,
+						colors=[lang['color'][i], ft.colors.YELLOW ]
+						)
+					)
+				)
+		return items
 
-	element1 = ft.Text('This is element 1')
-	element2 = ft.TextField(label='This is element 2', on_change=show_element3)
-	element3 = ft.Text()
-
+	"""elements"""
+	container = ft.Column(spacing=0, controls=items(len(lang['code'])))
+	main_container = ft.Container(content=container, bgcolor='Black', border_radius=20)
 
 	page.update()
-	page.add(element1, element2, element3)
 
+	"""layout"""
+	page.add(main_container)
 
 ft.app(target=main)
-# ft.app(target=main, view=ft.WEB_BROWSER)
