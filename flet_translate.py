aabfa87a5698235dec68d8bdacdd51f60e1cedb3
clueple <clueple@gmail.com>
2023-01-09 20:09:14 +0800

    Add files via upload
    A template to practice Flet Python - Translator project

--- flet_translate.py
+++ flet_translate.py
@@ -1,62 +1,64 @@
-import flet as ft
-from time import sleep
+# import flet 
+from flet import *
 from pyjyutping import jyutping as jtc 
-from tabulate import tabulate as tb 
-from tabulate import tabulate_formats
-import re 
 from googletrans import Translator
-from flet import colors
+import re 
 
+pat = r'[-a-zA-Z0-9]+|[\S]'
 
-#pattern to split words
-pat = "[-a-zA-Z0-9]+[\S]|[,:'?!.]"
-
-"""translator object"""
+#translator object for human language translation
 trans = Translator()
 
-"""backend setting """
-title_txt = "Warren's Translator"
-
-lang = {
-    'name': ['English', 'Italian', 'German', 'French', 'Japanese', 'Chinese', 'Spanish'],
-    "code": ['en', 'it', 'de', 'fr', 'ja', 'zh-CN', 'es'],
-    "color": ['red','purple', 'black', 'light blue', 'dark blue', 'green', 'brown'],
-    "dname": ['English','Italiano', 'Deutsch', 'Français', '日本語', '廣東話', 'Español']
+lang ={
+	'code': ['en', 'it', 'de', 'ja', 'fr', 'zh-TW', 'es'],
+	'dname': ['English', 'Italiano', 'Deutsch', '日本語', 'Français', '廣東話', 'Español'],
+	'name':['English', 'Italian', 'German', 'Japanese', 'French', 'Cantonese', 'Spanish'],
+	'color':['red', 'purple', 'black', "#211790", 'blue', 'green', 'brown']
 }
 
 
-def main(page: ft.Page):
-    page.theme_mode = 'dark'
-    page.window_width = 600
 
-    def get_result(e):
-        eng_trans.value = trans.translate(src_text.value , dest='en').text
-        bot_list = re.findall(pat, src_text.value)
-        top_list = [ trans.translate(bot_list[i], dest='en').text  for i in range(len(bot_list))]
-        table = [top_list, bot_list]
-        result = tb(table, tablefmt='tsv')
-        result_txt.value = result
+def main(page: Page):
+	page.title = "Warern's App"
+	page.vertical_alignment = "center"
+	page.window_height = 108*6
+	page.window_width = 108*6
 
-        src_lang = trans.detect(src_text.value).lang
-        src_lang_idx = lang['code'].index(src_lang)
-        result_txt.label = lang['dname'][src_lang_idx]
+	"""events"""
+	def get_eng(e):
+		try:
+			eng_trans.value = trans.translate(e.control.value,'en').text
+		except:
+			eng_trans.value = "Please try again"
+		page.update()
 
-        page.update()
-
-    eng_trans = ft.Text()
-
-
-    # result_txt = ft.Text()
-    result_txt = ft.TextField(label='Result',multiline=True, text_align='right')
-    src_text = ft.TextField(label='Enter Text', on_change=get_result)
-
-
-
-    """translation result"""    
-    
-    """layout"""
-    page.add(src_text, eng_trans, result_txt)
+	def get_lang(e):
+		result.controls = [TextField(label=lang['dname'][i], value=trans.translate(eng_trans.value,dest=item).text) for i, item in enumerate(lang['code'])]
+		page.update()
     
 
-# ft.app(target=main, view=ft.WEB_BROWSER)
-ft.app(target=main)
+	"""elements"""
+	entry = TextField(label = 'Enter text', on_change=get_eng)
+	eng_trans = TextField(label='English Translation', multiline=True)
+	btn_get_lang = ElevatedButton("Get Language", icon="select_all_rounded", on_click = get_lang)
+	# lang_labels = Column()
+	result = Column()
+
+	page.add(
+		Column(controls = [
+			#1st item of the main column
+			entry,
+			#2nd item of main column
+			eng_trans,
+			#3rd item of the main column
+			btn_get_lang,
+			# 4th item of the main column
+			# Row(controls=[lang_labels,result])
+            result
+			]
+			)
+		)
+	
+	page.update()
+
+app(target=main, view=WEB_BROWSER)
