b3d31fa6917f1efb0ab2e7f63bffe1193f60fa0d
clueple <clueple@gmail.com>
2023-01-20 19:54:57 +0800

    Add files via upload
    Template for practicing flet python app creation

--- flet_tut.py
+++ flet_tut.py
@@ -3,6 +3,7 @@ from pyjyutping import jyutping as jtc
 from pycantonese import segment 
 from googletrans import Translator
 import re 
+from tabulate import tabulate as tb 
 
 pat = r'[-a-zA-Z0-9]+|[\S]'
 
@@ -20,19 +21,27 @@ lang ={
 def main(page: Page):
 	"""background settings"""
 	page.window_width = 108*6
-	page.window_height = 135*6	
+	page.window_height = 135*6
+
+	"""create events"""
 
 	"""create elements"""
-	element1 = TextField(label="element 1")
-	element2 = TextField(label="element 2")
-
+	element1 = TextField(label="element 1", multiline=True)
+	element2 = TextField(label="element 2", expand=8)
+	element3 = ElevatedButton(text = "Get Result", expand=2)
 
 	"""design layout"""
 	page.add(
 		Container(
-			content=Column(controls=[element1, element2])
+			content=Column(
+				controls=[
+				element1,
+				element2
+				]
+				)
 			)
 		)
 
 
-app(target=main, view=WEB_BROWSER)
+# app(target=main, view=WEB_BROWSER)
+app(target=main)
