5b17b4894325f2f4452a96146e1877eebb7156c5
clueple <19367464+clueple@users.noreply.github.com>
2023-06-26 11:30:41 +0800

    Add files via upload
    This is the template for students to create their own bilingual subtitles ( .srt).
    Feel free to tweak the 'result' element and respective click events to suit your case

---
+++ nicegui_subtitle_translate_example.py
@@ -0,0 +1,27 @@
+"""python packages needed"""
+from nicegui import ui, events
+from deep_translator import GoogleTranslator as gt
+
+
+"""click events"""
+def handle_upload(e: events.UploadEventArguments):
+    """ When uploading a file, the preview element preview the file content """
+    preview.clear()
+    prev_txt = e.content.read().decode('utf-8')
+    preview.set_value(prev_txt)
+    preview.update()
+    # change the 'result' element for your case
+    result.set_value(preview.value)
+
+
+"""upload area"""
+src_file = ui.upload(auto_upload=True, on_upload=handle_upload)
+
+# text area to preview uploaded content
+preview = ui.textarea(label="Uploaded Content").style("width: 75%; height:200px ; border:solid 1px orange; resize: none; padding: 12px 12px")
+
+# text are
+result = ui.textarea(label="Translated Content").style("width: 75%; height:200px ; border:solid 1px orange; resize: none; padding: 12px 12px")
+  
+ui.update()
+ui.run()
