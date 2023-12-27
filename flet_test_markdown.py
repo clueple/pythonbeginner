b4d8aad5703a2ecd2ca202aeea5f1ea86c32c79e
clueple <19367464+clueple@users.noreply.github.com>
2023-12-27 21:36:52 +0800

    Add files via upload
    Flet test markdown template

--- flet_test_markdown.py
+++ flet_test_markdown.py
@@ -1,9 +1,12 @@
 import flet as ft 
-from flet import TextField, GridView, Page, Column, Row, ElevatedButton, Container, Markdown
+from flet import TextField, GridView, Page, Column, Row, ElevatedButton, Container, Markdown, Dropdown
 from deep_translator import GoogleTranslator as gt
 from nltk.tokenize import word_tokenize as wt 
 import nltk
 
+
+"""download template file here"""
+'https://github.com/clueple/pythonbeginner/blob/master/flet_test_markdown.py'
 
 """backend stuff"""
 trans = gt(source='auto', target='en')
@@ -30,6 +33,14 @@ def md_style() -> dict:
 	return {
 	'value': 'whatever',
 	'selectable': True,
+	}
+
+def drop_style() -> dict:
+	"""This style is going to be used for language selection dropdown list"""
+	return {
+	'options': [ft.dropdown.Option(i) for idx, i in enumerate(lang['dname'])],
+	'label': 'Target Language',
+	'hint_text': 'English',
 	}
 
 """main app"""
@@ -59,11 +70,14 @@ def main(page:Page):
 	"""elements"""
 	inp_txt = TextField(**txt_area_style(), label='Source Text', hint_text='Enter sentence here to be translated', on_change=show_result)
 	result = Markdown(**md_style())
-
+	# lang_select = Dropdown(options= [ft.dropdown.Option(i) for idx, i in enumerate(lang['dname'])])
+	lang_select = Dropdown(**drop_style())
 	
 
 	"""layout"""
 	page.add(
+		#item 0 - dropdown menu for language selection
+		lang_select,
 		Row(
 			controls=[
 			#item 1  - input text area
