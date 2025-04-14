124c180b5fd90259e23e5c7e20ead1214ee0887c
clueple <19367464+clueple@users.noreply.github.com>
2025-04-14 21:18:46 +0800

    Add files via upload
    NLLB-200 example on Windows; need to download the nllb-200 model with the python script, 'download_nllb200.py' before running this example

---
+++ download_nllb200.py
@@ -0,0 +1,5 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+model_name = "facebook/nllb-200-distilled-600M"
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
